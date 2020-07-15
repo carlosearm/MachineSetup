09d41f06d63a54fca69e7b42b61be5925446e3aa
carlosearm <carlosearm@gmail.com>
2020-07-15 15:20:19 -0400

    Update devtools.ps1
    Changed SQL package to mssqlexpress2014sp1wt with INSTANCEID=MSSQLSERVER, trying to install it as default instance.

--- scripts/devtools.ps1
+++ scripts/devtools.ps1
@@ -3,5 +3,5 @@ choco install -y sysinternals
 choco install beyondcompare
 choco install putty
 choco install mssql2014express-defaultinstance
-choco install sql-server-management-studio
+choco install mssqlexpress2014sp1wt -params '"/INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER"' -y
 choco install adobereader
