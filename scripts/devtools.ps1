29ec6a4cd8c4ada1f868ef0f414e3c623c1a1c6c
carlosearm <carlosearm@gmail.com>
2020-07-15 15:22:05 -0400

    Update devtools.ps1
    Added back SQL Management Studio that was removed by mistake.

--- scripts/devtools.ps1
+++ scripts/devtools.ps1
@@ -2,6 +2,6 @@ choco install -y vscode
 choco install -y sysinternals
 choco install beyondcompare
 choco install putty
-choco install mssql2014express-defaultinstance
 choco install mssqlexpress2014sp1wt -params '"/INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER"' -y
+choco install sql-server-management-studio
 choco install adobereader
