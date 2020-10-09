17b6752cce470b77eed435b5e1647398f9dbc810
carlosearm <carlosearm@gmail.com>
2020-10-09 10:24:26 -0400

    Update devtools.ps1
    Added parameter --cacheLocation="$ChocoCachePath" to avoid long paths on installs.

--- scripts/devtools.ps1
+++ scripts/devtools.ps1
@@ -1,8 +1,13 @@
-choco install dotnetfx
-choco install -y vscode
-choco install -y sysinternals
-choco install beyondcompare
-choco install putty
+# Workaround choco / boxstarter path too long error
+# https://github.com/chocolatey/boxstarter/issues/241
+$ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
+New-Item -Path $ChocoCachePath -ItemType Directory -Force
+
+choco install --cacheLocation="$ChocoCachePath" dotnetfx
+choco install --cacheLocation="$ChocoCachePath" -y vscode
+choco install --cacheLocation="$ChocoCachePath" -y sysinternals
+choco install --cacheLocation="$ChocoCachePath" beyondcompare
+choco install --cacheLocation="$ChocoCachePath" putty
 #choco install mssqlexpress2014sp1wt -params "'/INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER'" -y
 #choco install sql-server-management-studio
-choco install adobereader
+choco install --cacheLocation="$ChocoCachePath" adobereader
