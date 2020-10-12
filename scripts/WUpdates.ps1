92f4f6e8c6ccc38dc2aa52a0a1d89c9762006e5d
carlosearm <carlosearm@gmail.com>
2020-10-12 11:16:57 -0400

    Update WUpdates.ps1

--- scripts/WUpdates.ps1
+++ scripts/WUpdates.ps1
@@ -1,1 +1,7 @@
+
+# Workaround choco / boxstarter path too long error
+# https://github.com/chocolatey/boxstarter/issues/241
+$ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
+New-Item -Path $ChocoCachePath -ItemType Directory -Force
+
 choco install --cacheLocation="$ChocoCachePath" pswindowsupdate -y
