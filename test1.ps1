d45aee6b91962c720a5ad0d3a4497380819076b9
Liliana <lgutierrezh79@gmail.com>
2022-07-11 16:44:43 -0400

    v

--- test1.ps1
+++ test1.ps1
@@ -1,2 +1,30 @@
-#Add Windows Credential
-iex ((new-object net.webclient).DownloadString("https://raw.githubusercontent.com/liligut/MachineSetup/master/scripts/AddWindowsCredentials.ps1"))
+Disable-UAC
+$ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$strpos = $helperUri.LastIndexOf("/")
+$helperUri = $helperUri.Substring(0, $strpos)
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+$path = 'HKLM:\Software\Policies\Microsoft\Windows\PowerShell'
+$key = Get-Item -LiteralPath $path -ErrorAction SilentlyContinue
+if ($key -eq $null)
+{
+    (get-item HKLM:\Software\Policies\Microsoft).OpenSubKey("Windows", $true).CreateSubKey("PowerShell")
+}
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -Name "EnableScripts" -Value 00000001 -Type DWORD
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -Name "ExecutionPolicy" -Value "Unrestricted"
+executeScript "AddWindowsCredentials.ps1";
