acaa9506451eed5befd4196d0cdc403aa0e1fd07
Liliana <lgutierrezh79@gmail.com>
2022-07-11 13:38:24 -0400

    change test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,3 +1,17 @@
+Disable-UAC
+$ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$strpos = $helperUri.LastIndexOf("/")
+$helperUri = $helperUri.Substring(0, $strpos)
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
 
 function executeScript {
     Param ([string]$script)
