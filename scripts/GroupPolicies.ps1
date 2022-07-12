ac9dc0c7238b5dd39107029a11651b534196643a
Liliana <lgutierrezh79@gmail.com>
2022-07-12 17:09:43 -0400

    test

--- scripts/GroupPolicies.ps1
+++ scripts/GroupPolicies.ps1
@@ -1,4 +1,4 @@
 $ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
-New-Item -Path $ChocoCachePath -ItemType Directory -Force
+#New-Item -Path $ChocoCachePath -ItemType Directory -Force
 
 choco install --cacheLocation="$ChocoCachePath" -y rsat
