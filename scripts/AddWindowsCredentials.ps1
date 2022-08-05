96389f4b54c95375429f5dbff231a36f361a2546
Liliana <lgutierrezh79@gmail.com>
2022-08-05 11:53:25 -0400

    added windows credential script

--- scripts/AddWindowsCredentials.ps1
+++ scripts/AddWindowsCredentials.ps1
@@ -1,3 +1,3 @@
 Install-Module -Name CredentialManager -Force
-New-StoredCredential -Target "\\90.0.0.20" -UserName "Administrator" -Password "Administrator" -Persist Enterprise
+New-StoredCredential -Target "90.0.0.20" -UserName "Administrator" -Password "Administrator" -Persist Enterprise -Type DomainPassword
 #New-StoredCredential -Target "90.0.0.20\Public\Trajectories" -UserName "Administrator" -Password "Administrator" -Persist Enterprise
